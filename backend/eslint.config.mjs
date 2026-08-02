f8982cbce8ccac848cce214555f51cd2ea7b4816
Mukteswar Tripathy <mukteswart5@gmail.com>
2026-08-02 06:52:49 +0530

    feat(backend): initialize backend foundation

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {
+    ignores: ["dist", "node_modules", "prisma.config.ts"],
+  },
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "no-console": "off",
+    },
+  },
+];
